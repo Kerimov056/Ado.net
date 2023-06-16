e580ff4eb0faf7d58a88fced09c0a19d9616849e
Ulvi S. Karimov <ulvisk@code.local>
2023-06-16 21:54:36 +0400

    two

--- New folder/Ado/Ado/Program.cs
+++ New folder/Ado/Ado/Program.cs
@@ -1,16 +1,22 @@
 ﻿
 using System.Data.SqlClient;
+using System.Net;
+using System.Xml.Linq;
+
 
 var coonection = "Server=JUPITER18; Database=libaryDB; Trusted_Connection=True;";
 
-//GetAll();
-//SetData("Heyat Bilgisi", 60);
-//SearchByName("d");
+//GetAllBook();
+//SetData("sas", 60);
+//SearchByName("ed");
 //UpdateRow("AnaDili", "bayalogiya");
-//DeleteRow("AnaDili");
+//DeleteRow("ss");
+//SearchBookISBN("1r");
+//SearchUserNumber("055-555-55-55");
+//BorrowBook(11,1, "Riyaziyyat","1r","2023-10-01","2023-11-01");
+GetBorrowings();
 
-
-void GetAll()
+void GetAllBook()
 {
     string query = "SELECT * FROM Books";
 	using (SqlConnection conn = new SqlConnection(coonection))
@@ -59,6 +65,7 @@ void SetData(string name,int pageCount)
 
 void SearchByName(string name)
 {
+	if (name.Length < 2) { Console.WriteLine("Enter more than 2 letter"); return; }
 	string query = $"SELECT * FROM Books WHERE book_name like '%{name}%'";
 	using (SqlConnection conn = new SqlConnection(coonection))
 	{
@@ -124,4 +131,139 @@ void DeleteRow(string deleteName)
 		conn.Close();
 	}
 }
+
+void SearchBookISBN(string isbn)
+{
+    string query = $"SELECT * FROM Books WHERE book_isbn='{isbn}'";
+    using (SqlConnection conn = new SqlConnection(coonection))
+    {
+        try
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID:{reader[0]}; Name: {reader[1]}; PageCount: {reader[2]}; ISBN: {reader[3]}");
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        conn.Close();
+    }
+}
+
+void SearchUserNumber(string number)
+{
+    string query = $"SELECT * FROM Users WHERE phone_number='{number}'";
+    using (SqlConnection conn = new SqlConnection(coonection))
+    {
+        try
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID:{reader[0]}; Name: {reader[1]}; Surname: {reader[2]}; Phone_number: {reader[3]}; mail_address: {reader[4]}");
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        conn.Close();
+    }
+} //USERSS
+
+
+
+void BorrowBook(int bookID,int userID,string book_name,string book_isbn,string boorowing_date,string return_date) //borrowing
+{
+	
+	var query = $"INSERT INTO Borrowings values('{bookID}','{userID}','{book_name}','{book_isbn}','{boorowing_date}','{return_date}')";
+	using(SqlConnection conn = new SqlConnection(coonection))
+	{
+		try
+		{
+			SqlCommand cmd = new SqlCommand(query, conn);
+			conn.Open();
+			cmd.ExecuteNonQuery();
+			Console.WriteLine("Succesfuly Created");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		conn.Close();
+	}
+}
+
+void GetBorrowings()
+{
+    string query = "SELECT * FROM Borrowings";
+    using (SqlConnection conn = new SqlConnection(coonection))
+    {
+        try
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader[0]} BookID:{reader[1]} UserID: {reader[2]} BookName: {reader[3]} BookIsbn: {reader[4]} BorrowingDate: {reader[5]} ReturnDate: {reader[6]}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        conn.Close();
+    }
+
+} //isyahini cixardir
+
+
+//string ibsnReturn()
+//{
+//	string [] arrISBN; 
+
+//    string query = "SELECT * FROM Borrowings";
+//    using (SqlConnection conn = new SqlConnection(coonection))
+//    {
+//        try
+//        {
+//            SqlCommand cmd = new SqlCommand(query, conn);
+//            conn.Open();
+//            SqlDataReader reader = cmd.ExecuteReader();
+//            if (reader.HasRows)
+//            {
+//                while (reader.Read())
+//                {
+//                    Console.WriteLine($"ID: {reader[0]} BookID:{reader[1]} UserID: {reader[2]} BookName: {reader[3]} BookIsbn: {reader[4]} BorrowingDate: {reader[5]} ReturnDate: {reader[6]}");
+//                }
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine(ex.Message);
+//        }
+//        conn.Close();
+//    }
+//} 
+
+
 
--- New folder/SQLQuery1.sql
+++ New folder/SQLQuery1.sql
@@ -15,17 +15,21 @@ CREATE TABLE Books(
 [book_name] NVARCHAR(255),
 [page_count] INT,
 )
+alter table Books 
+add [book_isbn] nvarchar(255) unique
 
 CREATE TABLE Borrowings(
 [Borrowings_id] INT IDENTITY PRIMARY KEY,
 [book_id] INT FOREIGN KEY REFERENCES Books([book_id]),
 [user_id] INT FOREIGN KEY REFERENCES Users([user_id]),
 [book_name] NVARCHAR(255),
+[book_isbn] nvarchar(255) unique FOREIGN KEY REFERENCES Books([book_isbn]),
 [borrowing_date] date,
 [return_date] date,
 )
 
-SELECT * FROM Books
+
+SELECT * FROM Users
 
 insert into Books 
 values('Riyaziyyat',200);
@@ -36,3 +40,9 @@ SELECT * FROM Books WHERE book_name like '%r%'
 update Books set book_name='edebiyat' where book_name='AnaDili'
 
 delete from Books where book_name='Riyaziyat'
+
+SELECT * FROM Books WHERE book_isbn='1r'
+
+
+
+Select * from Borrowings 
